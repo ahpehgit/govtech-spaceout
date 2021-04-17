667bf1d4a4ef517c595c3e00e0b91f3b97ab5072
Ahpeh <ahpeh@hotmail.com>
2021-04-17 17:41:26 +0800

    CHANGE: Added EncryptionService

--- src/config/dependencies.js
+++ src/config/dependencies.js
@@ -1,7 +1,9 @@
 const InMemoryDBService = require('../infrastructure/database/InMemoryDBService');
+const AESEncryption = require('../infrastructure/security/AESEncryption');
 
 module.exports = (() => {
     return {
         DBService: new InMemoryDBService(),
+        EncryptionService: new AESEncryption(),
     };
 })();
